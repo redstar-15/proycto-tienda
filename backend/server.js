1f0ffefcd36e943e80c14de54088dbf8bcd36783
Jhoan <jhoanbejarano03@gmail.com>
2026-07-30 16:52:56 -0500

    arreglo server 2

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,9 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor iniciado en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en puerto ${PORT}`);
 });
